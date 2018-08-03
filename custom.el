16e7f5e5864b851217b19ca5487781af3077b1f1
aerian <wo199710@hotmail.com>
2018-08-03 22:59:23 +0800

    配置更新

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yapfify pytest pyenv-mode py-isort pip-requirements live-py-mode hy-mode helm-pydoc cython-mode company-anaconda anaconda-mode pythonic irony-eldoc company-irony-c-headers company-irony irony elpy swiper ws-butler winum which-key web-mode web-beautify volatile-highlights vimrc-mode vi-tilde-fringe uuidgen use-package toc-org tagedit spaceline powerline smartparens slim-mode scss-mode sass-mode haml-mode rvm ruby-tools ruby-test-mode rubocop rspec-mode robe restart-emacs rbenv ranger rake rainbow-mode rainbow-identifiers rainbow-delimiters pyim pyim-basedict pug-mode popwin persp-mode pcre2el paradox spinner pangu-spacing org-pomodoro alert log4e gntp org-mime open-junk-file neotree move-text mmm-mode minitest markdown-toc markdown-mode macrostep linum-relative link-hint less-css-mode json-mode json-snatcher json-reformat js2-refactor multiple-cursors js2-mode js-doc indent-guide hydra hungry-delete htmlize hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation helm-projectile projectile pkg-info epl helm-mode-manager helm-gitignore request helm-flx flx helm-descbinds helm-css-scss helm-company helm-ag helm helm-core golden-ratio gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link fuzzy find-by-pinyin-dired fill-column-indicator expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-magit magit magit-popup git-commit ghub let-alist with-editor evil-indent-plus evil-iedit-state iedit evil-exchange evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu highlight emmet-mode elisp-slime-nav dumb-jump popup f diminish dactyl-mode company-web web-completion-data company-tern s dash-functional tern company-statistics company column-enforce-mode color-identifiers-mode dash cmake-mode chruby bundler inf-ruby bind-map bind-key auto-yasnippet yasnippet auto-highlight-symbol auto-compile packed async aggressive-indent adaptive-wrap ace-window ace-pinyin pinyinlib ace-link avy))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
